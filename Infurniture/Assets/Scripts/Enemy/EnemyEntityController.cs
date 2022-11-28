d3ed32c37fb9ea1732943a4ecc7e7607db78976b
jjm0095 <54652378+jjm0095@users.noreply.github.com>
2022-11-27 22:32:37 -0600

    Added basic enemy pathfinding

--- Infurniture/Assets/Scripts/Enemy/EnemyEntityController.cs
+++ Infurniture/Assets/Scripts/Enemy/EnemyEntityController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyEntityController : MonoBehaviour
 {
+    NavMeshAgent agent;
+
     public GameObject playerObject;
     public float directionFacing;
     public float m_speed;
@@ -15,6 +18,9 @@ public class EnemyEntityController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     void FixedUpdate()
@@ -22,15 +28,20 @@ public class EnemyEntityController : MonoBehaviour
         
         distance = Vector2.Distance(transform.position, playerObject.transform.position);
         Vector2 direction = playerObject.transform.position - transform.position;
-        if(direction.x > 0){
-            GetComponent<SpriteRenderer>().flipX = true;
-        }else{
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
         if(distance < AggroDistance){
-            transform.position = Vector2.MoveTowards(this.transform.position, playerObject.transform.position, m_speed * Time.deltaTime);
+            agent.SetDestination(playerObject.transform.position);
+            //transform.position = Vector2.MoveTowards(this.transform.position, playerObject.transform.position, m_speed * Time.deltaTime);
             animator.SetFloat("velocity", 1);
-        }else{
+            if (direction.x > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+        else{
             animator.SetFloat("velocity", 0);
         }
     }
